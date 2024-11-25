e83c3f541e4994f476fa714a52a80892e3ed979d
noppiii <mnoviantoanggoro@gmail.com>
2024-11-25 19:54:12 +0700

    init project

---
+++ src/test/java/com/nutech/backend/NutechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutech.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
